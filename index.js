4053635f9f435dc8f7f06c7375153bbf74ca90cb
omondimaurine <omondimaurine713@gmail.com>
2024-03-12 12:28:31 +0300

    javascript function trial

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Follow along with the examples here
+function sayhello(firstname){
+    return "Hello" + " " + firstname;
+
+}
+console.log(sayhello("Maurine"));
+
+function multiply(num1,num2){
+    console.log("The result is:");
+    return num1*num2;
+}
+console.log(multiply(76,45));
